f646d76ea827150cf404c2aa2d16e519ae68760d
duc toan <ductoan0131@gmail.com>
2026-07-02 22:41:24 +0700

    RealmType.cs

---
+++ RealmType.cs
@@ -0,0 +1,23 @@
+namespace ImmortalIdle.Cultivation
+{
+    public enum RealmType
+    {
+        Mortal,
+
+        QiRefining,
+
+        Foundation,
+
+        CoreFormation,
+
+        NascentSoul,
+
+        SoulTransformation,
+
+        Void,
+
+        Tribulation,
+
+        Immortal
+    }
+}
